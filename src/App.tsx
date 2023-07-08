243db93e4d733ba35fe10756b11a6064ed315f5e
bilecky <bilecky95@gmail.com>
2023-07-08 14:16:26 +0200

    added background shape & auth structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import About from './components/About'
 import Home from './components/home/Home'
+import Register from './components/auth/register'
 
 // import { useEffect } from 'react';
 // import { collection, addDoc } from 'firebase/firestore';
@@ -11,9 +12,11 @@ const App = () => {
 	return (
 		<BrowserRouter>
 			<Navbar />
+			<img src='/src/assets/png-image.png' alt=''  className='top-0 opacity-30  -z-10 absolute w-full h-full overflow-hidden'/>
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/about' element={<About />} />
+				<Route path='/about' element={<Register />} />
 			</Routes>
 		</BrowserRouter>
 	)
--- src/firebase/firebaseConfig.tsx
+++ src/firebase/firebaseConfig.tsx
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -24,3 +25,5 @@ const analytics = getAnalytics(app);
 
 // Initialize Cloud Firestore and get a reference to the service
 export const database = getFirestore(app);
+
+export const auth = getAuth(app);
